6dc21bf6161a58379b67970fa806490f84c88f7f
ChristYoga123 <christianuswibisono@gmail.com>
2024-09-14 22:00:16 +0700

    fix: menu schema

--- database/migrations/2024_09_13_012545_create_menus_table.php
+++ database/migrations/2024_09_13_012545_create_menus_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('menus', function (Blueprint $table) {
             $table->id();
             $table->string('key');
+            $table->primary('key');
             $table->string('nama_menu');
             $table->string('parent')->nullable();
             $table->foreign('parent')->references('key')->on('menus')->onDelete('cascade');
