3842c2021ca2b7faa21515ad3e89894a88a39701
ChristYoga123 <christianuswibisono@gmail.com>
2024-09-20 13:14:17 +0700

    fix: fix button

--- resources/views/components/front/header.blade.php
+++ resources/views/components/front/header.blade.php
@@ -16,8 +16,26 @@
                     <button class="bg-success cta-btn d-sm-block" data-bs-toggle="modal"
                         data-bs-target="#exLargeModal">Daftar</button>
                 </li>
-                <li class="mt-sm-3 mt-md-0">
-                    <button onclick="redirectDashboard()" class="bg-danger cta-btn d-sm-block">Masuk</button>
+                <li>
+                    {{-- @dd(Auth::user()->getRoleNames()) --}}
+                    @if (Auth::user()->hasRole('Administrator'))
+                        <a href="{{ route('admin.dashboard.index') }}"
+                            class="cta-btn d-sm-block bg-danger">Dashboard</a>
+                    @elseif (Auth::user()->hasRole('Pemohon'))
+                        <a href="{{ route('peserta.dashboard.index') }}"
+                            class="cta-btn d-sm-block bg-danger">Dashboard</a>
+                    @elseif (Auth::user()->hasRole('Pembimbing'))
+                        <a href="{{ route('pembimbing.dashboard.index') }}"
+                            class="cta-btn d-sm-block bg-danger">Dashboard</a>
+                    @elseif(Auth::user()->hasRole('Pimpinan'))
+                        <a href="{{ route('admin.dashboard.index') }}"
+                            class="cta-btn d-sm-block bg-danger">Dashboard</a>
+                    @elseif (Auth::user()->hasRole('Operator'))
+                        <a href="{{ route('admin.dashboard.index') }}"
+                            class="cta-btn d-sm-block bg-danger">Dashboard</a>
+                    @else
+                        <button class="cta-btn d-sm-block bg-danger" onclick="redirectDashboard()">Masuk</button>
+                    @endif
                 </li>
             </ul>
             <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
