dd2461136af28a3028af748646d0b09e1468d0a9
ChristYoga123 <christianuswibisono@gmail.com>
2024-09-23 12:01:30 +0700

    feat: admin dashboard

--- app/Http/Controllers/Back/Admin/DashboardController.php
+++ app/Http/Controllers/Back/Admin/DashboardController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Back\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Kegiatan;
+use App\Models\Pemohon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.back.admin.dashboard.index');
+        $pemohon = Pemohon::count();
+        $riset = Kegiatan::whereJenisKegiatan('Riset')->count();
+        $kkp = Kegiatan::whereJenisKegiatan('KKP')->count();
+        $prakerin = Kegiatan::whereJenisKegiatan('Prakerin')->count();
+        // kirimkan data ke chartjs
+        $kegiatan = Kegiatan::selectRaw('jenis_kegiatan, count(*) as total')
+            ->groupBy('jenis_kegiatan')
+            ->get();
+        return view('pages.back.admin.dashboard.index', compact('pemohon', 'riset', 'kkp', 'prakerin', 'kegiatan'));
     }
 }
