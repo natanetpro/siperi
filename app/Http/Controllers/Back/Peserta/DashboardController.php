825ec256e9e212656db1d6506a1fde652810d8e6
ChristYoga123 <christianuswibisono@gmail.com>
2024-09-23 12:32:14 +0700

    feat: peserta dashboard

--- app/Http/Controllers/Back/Peserta/DashboardController.php
+++ app/Http/Controllers/Back/Peserta/DashboardController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Back\Peserta;
 
 use App\Http\Controllers\Controller;
+use App\Models\Logbook;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.back.peserta.dashboard.index');
+        $menunggu = Logbook::whereApprovalPembimbing('Menunggu')->whereHas('userKegiatan', function ($query) {
+            $query->where('user_id', auth()->user()->id);
+        })->count();
+        $diterima = Logbook::whereApprovalPembimbing('Disetujui')->whereHas('userKegiatan', function ($query) {
+            $query->where('user_id', auth()->user()->id);
+        })->count();
+        $ditolak = Logbook::whereApprovalPembimbing('Ditolak')->whereHas('userKegiatan', function ($query) {
+            $query->where('user_id', auth()->user()->id);
+        })->count();
+        return view('pages.back.peserta.dashboard.index', compact('menunggu', 'diterima', 'ditolak'));
     }
 }
